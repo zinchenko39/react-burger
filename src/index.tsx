7bde19b798ad1e8247b7f8bfbdaa4fd5b4ffd649
Vadim <vadim.zinchenco@mail.ru>
2022-10-09 04:30:47 +0200

    Changed to React 17

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import App from './components/app/App.js';
 import './css/index.css';
 import { store } from './services/store.js';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
